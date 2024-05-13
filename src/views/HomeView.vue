2c7444fc8277600b1fdd4224552d046e7ab8418a
Nikola Smiljanic <nikolasmiljanic65@gmail.com>
2024-05-13 10:03:57 +0200

    products on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,14 +4,37 @@
       <h2>Filteri</h2>
       <!-- Dodajte filtere ovde -->
     </aside>
-
     <main>
+      <div id="div-search">
+        <input type="text" placeholder="Pretraga...">
+        <button>Pretraži</button>
+      </div>
       <div class="products">
-        <!-- Dodajte prikaz proizvoda ovde -->
-        <div id="div-search">
-      <input type="text" placeholder="Pretraga...">
-      <button>Pretraži</button>
-    </div>
+        <div class='product'>
+          <img src="/majica1.jpg" alt="Proizvod 1">
+          <h2>Naziv proizvoda 1</h2>
+          <p>Opis proizvoda 1</p>
+          <p>Cena: $20</p>
+          <p>Veličina: M</p>
+          <button>Kupi</button>
+        </div>
+        <!-- Dodajte ostale proizvode -->
+        <div class='product'>
+          <img src="./majica2.jpg" alt="Proizvod 2">
+          <h2>Naziv proizvoda 2</h2>
+          <p>Opis proizvoda 2</p>
+          <p>Cena: $30</p>
+          <p>Veličina: L</p>
+          <button>Kupi</button>
+        </div>
+        <div class='product'>
+          <img src="./pantalone.jpg" alt="Proizvod 3">
+          <h2>Naziv proizvoda 3</h2>
+          <p>Opis proizvoda 3</p>
+          <p>Cena: $40</p>
+          <p>Veličina: S</p>
+          <button>Kupi</button>
+        </div>
       </div>
     </main>
   </div>
