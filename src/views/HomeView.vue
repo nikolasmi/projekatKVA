a09cc0599e219f66dba41d90c66d6e7fd2898d49
Nikola Smiljanic <nikolasmiljanic65@gmail.com>
2024-05-14 11:16:31 +0200

    items from database

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,50 +1,60 @@
 <template>
   <div class="container">
+    <div id="div-search">
+        <input type="text" placeholder="Pretraga...">
+        <button>Pretraži</button>
+      </div>
     <aside>
       <h2>Filteri</h2>
       <!-- Dodajte filtere ovde -->
     </aside>
     <main>
-      <div id="div-search">
-        <input type="text" placeholder="Pretraga...">
-        <button>Pretraži</button>
-      </div>
-      <div class="products">
-        <div class='product'>
+        <div class="product" v-for="item in items" :key="item.itemId">
+          <img :src="`/${item.picturePath}`" :alt="`${item.picturePath}`">
+          <h2>{{ item.name }}</h2>
+          <p>Opis proizvoda 1</p>
+          <p>Cena: {{ item.price }}</p>
+          <p>Veličina: {{ item.size }}</p>
+          <button>Kupi</button>
+        </div>
+        <!-- <div class='product'>
           <img src="/majica1.jpg" alt="Proizvod 1">
           <h2>Naziv proizvoda 1</h2>
           <p>Opis proizvoda 1</p>
           <p>Cena: $20</p>
           <p>Veličina: M</p>
           <button>Kupi</button>
-        </div>
-        <!-- Dodajte ostale proizvode -->
-        <div class='product'>
-          <img src="./majica2.jpg" alt="Proizvod 2">
-          <h2>Naziv proizvoda 2</h2>
-          <p>Opis proizvoda 2</p>
-          <p>Cena: $30</p>
-          <p>Veličina: L</p>
-          <button>Kupi</button>
-        </div>
-        <div class='product'>
-          <img src="./pantalone.jpg" alt="Proizvod 3">
-          <h2>Naziv proizvoda 3</h2>
-          <p>Opis proizvoda 3</p>
-          <p>Cena: $40</p>
-          <p>Veličina: S</p>
-          <button>Kupi</button>
-        </div>
-      </div>
+        </div> -->
     </main>
   </div>
-
   <footer>
     <p>&copy; 2024 Mili Shop. Sva prava zadržana.</p>
   </footer>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import axios from 'axios';
+import type { Item } from '@/types/Item';
+const items = ref<Item[]>([]);
+
+const data = ref({
+  name: '',
+  type: '',
+  size: '',
+  manufacturer: '',
+  dateAdded: '',
+  price: null,
+  picturePath: ''
+});
+
+(async () => {
+  try {
+    const { data: allItems } = await axios.get<Item[]>('http://localhost:5104/api/Item/GetItems');
+    items.value = allItems
+  } catch (e) {
+  }
+})() //imidiatly envoke function
 // import { ref, onMounted } from 'vue';
 // import axios from 'axios';
 // import type { User } from '@/types/User';
