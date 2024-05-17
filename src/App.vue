ae749fbfb1df5b40cf1b269a1b9b7054d3e77d42
Nikola Smiljanic <nikolasmiljanic65@gmail.com>
2024-05-17 10:43:09 +0200

    filter on home page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Header />
   <RouterView />
+  <Footer />
 </template>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer>
+    <p>&copy; 2024 Mili Shop. Sva prava zadržana.</p>
+  </footer>
+</template>
+
+<style scoped>
+    @import "../assets/home.css";
+</style>
